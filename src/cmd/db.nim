595f33416bfb4d2b8715aabe9639fd3523f989a1
froehlichA <froehlich.alexander@protonmail.com>
2020-12-13 22:48:18 +0100

    implement update-db

--- src/cmd/db.nim
+++ src/cmd/db.nim
@@ -1,5 +1,8 @@
-import ../lib/io/files
+import ../lib/io/files, ../lib/io/http
 
 proc cmdUpdateDb*(): void =
   ## update local databases of available forge versions
   requirePaxProject
+
+  echoInfo "Updating databases.."
+  writeFile(forgeVersionFile, fetch(forgeVersionUrl))
