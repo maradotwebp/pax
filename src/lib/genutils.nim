e41abcb2b7f7bcbe58271fcad32ddb5125320f9f
froehlichA <froehlich.alexander@protonmail.com>
2020-12-17 23:18:51 +0100

    format genutils correctly

--- src/lib/genutils.nim
+++ src/lib/genutils.nim
@@ -1,11 +1,11 @@
 proc `??`*[A](x: A, y: A): A =
-    ## returns y if x is not defined
-    if x is nil: y else: x
+  ## returns y if x is not defined
+  if x is nil: y else: x
 
 proc `??`*(x: string, y: string): string =
-    ## return y if x is not defined
-    if x == "": y else: x
+  ## return y if x is not defined
+  if x == "": y else: x
 
 proc `?`*[A](c: bool, t: (A, A)): A =
-    ## return the first element if condition is true, otherwise the second
-    if c: t[0] else: t[1]
+  ## return the first element if condition is true, otherwise the second
+  if c: t[0] else: t[1]
