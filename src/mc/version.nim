6a6fe817eaf460add8635b65506a654466960381
Alexander Fröhlich <froehlich.alexander@protonmail.com>
2021-06-14 00:59:39 +0200

    fix `mc/version`

--- src/mc/version.nim
+++ src/mc/version.nim
@@ -1,4 +1,4 @@
-import hashes, sequtils, strutils, options
+import hashes, sequtils, strutils
 
 type
   ## A minecraft version (1.12.2, 1.16.4, 1.14.1, ...)
@@ -6,22 +6,6 @@ type
   Version* = distinct string
 
 proc `$`*(v: Version): string {.borrow.}
-proc `==`*(v1: Version, v2: Version): bool {.borrow.}
-
-proc hash*(x: Version): Hash = ($x).hash
-
-proc `>`*(v1: Version, v2: Version): bool =
-  ## compares two versions
-  let v1Parts = ($v1).split('.')
-  let v2Parts = ($v2).split('.')
-  # Snapshots
-  if v1Parts.len == 2 and v1Parts[1].contains("-Snapshot"): return false
-  if v2Parts.len == 2 and v2Parts[1].contains("-Snapshot"): return true
-  # 1.16.4 > 1.16
-  if v1Parts.len == 2 or v2Parts.len == 2: return v1Parts[1].parseInt > v2Parts[1].parseInt
-  # match by minor version first, then by patch
-  if v1Parts[1].parseInt == v2Parts[1].parseInt: return v1Parts[2].parseInt > v2Parts[2].parseInt
-  return v1Parts[1].parseInt > v2Parts[1].parseInt
 
 proc minor*(v: Version): string =
   ## get the minor part of a version. "1.16.4" -> "1.14"
@@ -34,16 +18,30 @@ proc minor*(v: Version): string =
   # if format's good, just join the first 2 parts
   return parts[0] & "." & parts[1]
 
-proc newest*(v: seq[Version], match: Version): Option[Version] =
-  ## get the newest version from a sequence that is compatible with match
-  result = none(Version)
-  for item in v:
-    if result.isNone:
-      if minor(item) == minor(match):
-        result = some(item)
-    if result.isSome:
-      if item > result.get() and minor(item) == minor(match):
-        result = some(item)
+proc `==`*(v1: Version, v2: Version): bool {.borrow.}
+
+proc hash*(x: Version): Hash = ($x).hash
+
+proc `>`*(v1: Version, v2: Version): bool =
+  ## compares two versions
+  let v1Parts = ($v1).split('.')
+  let v2Parts = ($v2).split('.')
+  # Snapshots
+  if v1Parts.len == 2 and v1Parts[1].contains("-Snapshot"):
+    if v1.minor == v2.minor: return false
+    return v1Parts[1].replace("-Snapshot").parseInt > v2Parts[1].replace("-Snapshot").parseInt
+  if v2Parts.len == 2 and v2Parts[1].contains("-Snapshot"):
+    if v1.minor == v2.minor: return true
+    return v1Parts[1].replace("-Snapshot").parseInt > v2Parts[1].replace("-Snapshot").parseInt
+  # 1.16.4 > 1.16
+  if v1Parts.len == 2 or v2Parts.len == 2: return v1Parts[1].parseInt > v2Parts[1].parseInt
+  # match by minor version first, then by patch
+  if v1Parts[1].parseInt == v2Parts[1].parseInt: return v1Parts[2].parseInt > v2Parts[2].parseInt
+  return v1Parts[1].parseInt > v2Parts[1].parseInt
+
+proc `<`*(v1: Version, v2: Version): bool = v2 > v1
+proc `>=`*(v1: Version, v2: Version): bool = v1 > v2 or v1 == v2
+proc `<=`*(v1: Version, v2: Version): bool = v1 < v2 or v1 == v2
 
 proc proper*(v: seq[Version]): seq[Version] =
   ## only return proper versions
