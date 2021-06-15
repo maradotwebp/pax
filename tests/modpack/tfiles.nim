588572b381c5bdf59165fe3fdd18d389bb43e5ba
Alexander Fröhlich <froehlich.alexander@protonmail.com>
2021-06-15 17:55:19 +0200

    add `modpack/files` tests

---
+++ tests/modpack/tfiles.nim
@@ -0,0 +1,89 @@
+discard """"""
+
+import mc/version, modpack/files, modpack/loader, random, os
+
+randomize()
+
+block: # manifest loader
+  var m: Manifest
+  m.name = "testmodpack"
+  m.author = "testauthor"
+  m.version = "1.0.0"
+  m.mcVersion = "1.16.3".Version
+
+  m.mcModloaderId = "fabric-0.11.0"
+  doAssert m.loader == Loader.fabric
+
+  m.mcModloaderId = "forge-34.1.0"
+  doAssert m.loader == Loader.forge
+
+block: # manifest mods
+  var m: Manifest
+  m.name = "testmodpack"
+  m.author = "testauthor"
+  m.version = "1.0.0"
+  m.mcVersion = "1.16.3".Version
+
+  doAssert m.files.len == 0
+
+  m.installMod(111, 200)
+  doAssert m.files.len == 1
+  doAssert m.files[0].projectId == 111
+  doAssert m.files[0].fileId == 200
+
+  m.updateMod(111, 300)
+  doAssert m.files.len == 1
+  doAssert m.files[0].projectId == 111
+  doAssert m.files[0].fileId == 300
+
+  m.removeMod(111)
+  doAssert m.files.len == 0
+
+block: # manifest write / read from disk
+  var m: Manifest
+  m.name = "testmodpack"
+  m.author = "testauthor"
+  m.version = "1.0.0"
+  m.mcVersion = "1.16.3".Version
+
+  let dirname = joinPath(".", $rand(1000))
+  createDir(dirname)
+  defer: removeDir(dirname)
+  let mpath = joinPath(dirname, "manifest.json")
+
+  m.writeToDisk(path = mpath)
+  let readM = readManifestFromDisk(path = mpath)
+  doAssert m == readM
+
+block: # requirePaxProject
+  let f = proc(): int =
+    result = -1
+    requirePaxProject()
+    result = 1
+  
+  removeDir(packFolder)
+
+  doAssert f() == -1
+
+  createDir(packFolder)
+  createDir(overridesFolder)
+  writeFile(manifestFile, "Hello pax test")
+
+  doAssert f() == 1
+
+block: # rejectPaxProject
+  let f = proc(): int =
+    result = -1
+    rejectPaxProject()
+    result = 1
+  
+  removeDir(packFolder)
+
+  doAssert f() == 1
+
+  createDir(packFolder)
+  createDir(overridesFolder)
+  writeFile(manifestFile, "Hello pax test")
+
+  doAssert f() == -1
+  
