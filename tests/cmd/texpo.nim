83297be8db095048130135878b547e4f2bbb7add
Alexander Fröhlich <froehlich.alexander@protonmail.com>
2021-07-31 20:55:07 +0200

    allow custom export locations

--- tests/cmd/texpo.nim
+++ tests/cmd/texpo.nim
@@ -41,11 +41,56 @@ block: # export .zip
   }
   createDir("./modpack")
   writeFile("./modpack/manifest.json", manifestJson.pretty)
-  paxExport()
+  paxExport(path = "")
 
   doAssert fileExists("./.out/testmodpack123.zip")
 
   removeDir("./modpack")
   extractAll("./.out/testmodpack123.zip", "./modpack")
+
+  doAssert parseFile("./modpack/manifest.json") == manifestJson
+
+block: # export .zip to custom location
+  removeDir("./.out/")
+  removeDir("./modpack/")
+
+  let manifestJson = %* {
+    "minecraft": {
+      "version": "1.16.5",
+      "modLoaders": [
+        {
+          "id": "forge-36.1.0",
+          "primary": true
+        }
+      ]
+    },
+    "manifestType": "minecraftModpack",
+    "overrides": "overrides",
+    "manifestVersion": 1,
+    "version": "1.0.0",
+    "author": "testauthor",
+    "name": "testmodpack123",
+    "files": [
+      {
+        "projectID": 238222,
+        "fileID": 3383214,
+        "required": true,
+        "__meta": {
+          "name": "Just Enough Items (JEI)",
+          "explicit": true,
+          "dependencies": []
+        }
+      }
+    ]
+  }
+  createDir("./modpack")
+  writeFile("./modpack/manifest.json", manifestJson.pretty)
+  paxExport(path = "./completely-custom-out-dir/somewhere/here.zip")
+  defer: removeDir("./completely-custom-out-dir/somewhere/")
+
+  doAssert fileExists("./completely-custom-out-dir/somewhere/here.zip")
+
+  removeDir("./modpack")
+  extractAll("./completely-custom-out-dir/somewhere/here.zip", "./modpack")
 
   doAssert parseFile("./modpack/manifest.json") == manifestJson
